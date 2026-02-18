70be84f6c8e98e4afb649482c99b83ecde83a5a7
Mohamed Fazil <mohamedfazil152@gmail.com>
2026-02-18 15:51:41 +0530

    base url changed

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,42 +1,40 @@
 // api/endpoints.js
 export class ApiEndpoints {
   // Base URL
-//   static BASE_URL = 'http://localhost:3333/api/v1';
   //static BASE_URL = 'https://krizianohome.com/';
    static BASE_URL = 'https://kriziano-homes.onrender.com/';
 
   // Product Service Endpoints
   static PRODUCT = {
-    GET_PRODUCTS: '/product/get-products',
-    GET_PRODUCT: '/product/get-product/:product_id',
-    GET_CATEGORIES: '/product/get-categories',
-    CREATE_PRODUCT: '/product/create-product',
-    UPDATE_PRODUCT: '/product/:id',
-    ADD_WISHLIST: '/product/add-wishlist',
-    ADD_CART: '/product/add-cart',
-    GET_CART: '/product/get-cart',
-    GET_WISHLISTS: '/product/get-wishlists',
-    REMOVE_CART: '/product/remove-cart',
-    REMOVE_WISHLIST: '/product/remove-wishlist',
+    GET_PRODUCTS: this.BASE_URL+'/product/get-products',
+    GET_PRODUCT: this.BASE_URL+'/product/get-product/:product_id',
+    GET_CATEGORIES:this.BASE_URL+ '/product/get-categories',
+    CREATE_PRODUCT: this.BASE_URL+'/product/create-product',
+    UPDATE_PRODUCT:this.BASE_URL+ '/product/:id',
+    ADD_WISHLIST: this.BASE_URL+'/product/add-cart',
+    GET_CART: this.BASE_URL+'/product/get-cart',
+    GET_WISHLISTS: this.BASE_URL+'/product/get-wishlists',
+    REMOVE_CART: this.BASE_URL+'/product/remove-cart',
+    REMOVE_WISHLIST:this.BASE_URL+ '/product/remove-wishlist',
   };
 
   // Auth Service Endpoints
   static AUTH = {
-    CREATE_USER: '/auth/create-user',
-    LOGIN: '/auth/login',
-    LOGOUT: '/auth/logout',
-    GET_USER: '/auth/get-user',
+    CREATE_USER:this.BASE_URL+ '/auth/create-user',
+    LOGIN: this.BASE_URL+'/auth/login',
+    LOGOUT:this.BASE_URL+ '/auth/logout',
+    GET_USER: this.BASE_URL+'/auth/get-user',
   };
 
   // Order Service Endpoints
   static ORDER = {
-    CREATE_ADDRESS: '/order/create-address',
-    GET_ADDRESS: '/order/get-address',
-    CREATE_ORDER: '/order/create-order',
-    VERIFY_PAYMENT: '/order/verify-payment',
-    GET_ORDERS: '/order/get-orders',
-    ADMIN_ORDERS: '/order/admin/orders',
-    ADMIN_UPDATE_STATUS: '/order/admin/order/:id/status',
+    CREATE_ADDRESS: this.BASE_URL+'/order/create-address',
+    GET_ADDRESS:this.BASE_URL+ '/order/get-address',
+    CREATE_ORDER: this.BASE_URL+'/order/create-order',
+    VERIFY_PAYMENT: this.BASE_URL+'/order/verify-payment',
+    GET_ORDERS: this.BASE_URL+'/order/get-orders',
+    ADMIN_ORDERS: this.BASE_URL+'/order/admin/orders',
+    ADMIN_UPDATE_STATUS: this.BASE_URL+'/order/admin/order/:id/status',
   };
 
   // Helper methods to build full URLs
